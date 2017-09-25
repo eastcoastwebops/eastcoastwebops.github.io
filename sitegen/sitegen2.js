82828b87fe42053f5c03593f9fdfbcffe4786096
work_PC <work_PC@thinkvacuums.com>
2017-09-25 14:33:36 -0400

    ipad again

--- sitegen/sitegen2.js
+++ sitegen/sitegen2.js
@@ -95,7 +95,39 @@ $(document).ready(function() {
       $("body").hide(0, function() {
          var content = 'sitegen/content/' + whichpage + '.html';
 				 $("#content").load(content, function() {
-					 
+
+
+
+
+;(function(){
+    var isTouch = false //var to indicate current input type (is touch versus no touch) 
+    var isTouchTimer 
+    var curRootClass = '' //var indicating current document root class ("can-touch" or "")
+     
+    function addtouchclass(e){
+        clearTimeout(isTouchTimer)
+        isTouch = true
+        if (curRootClass != 'can-touch'){ //add "can-touch' class if it's not already present
+            curRootClass = 'can-touch'
+            document.documentElement.classList.add(curRootClass)
+        }
+        isTouchTimer = setTimeout(function(){isTouch = false}, 500) //maintain "istouch" state for 500ms so removetouchclass doesn't get fired immediately following a touch event
+    }
+     
+    function removetouchclass(e){
+        if (!isTouch && curRootClass == 'can-touch'){ //remove 'can-touch' class if not triggered by a touch event and class is present
+            isTouch = false
+            curRootClass = ''
+            document.documentElement.classList.remove('can-touch')
+        }
+    }
+     
+    document.addEventListener('touchstart', addtouchclass, false) //this event only gets called when input type is touch
+    document.addEventListener('mouseover', removetouchclass, false) //this event gets called when input type is everything from touch to mouse/ trackpad
+})();
+
+
+				 
 /* doesn't work with ios safari so disable for now... 
 $('.full-bg-image.fix').each(function() {
 		var bg_img = $(this).css('background-image').replace(/^url\(['"](.+)['"]\)/, '$1').split('/').pop();
@@ -159,7 +191,7 @@ $(this).css('background-image','url("sitegen/site-images/'+bg_img+'")' );
 		 
  window.addEventListener('touchstart', function() {
   // the user touched the screen!
-	alert ('touched');
+
 });	
  
       var whichthis = $(this);
@@ -177,13 +209,13 @@ $(this).css('background-image','url("sitegen/site-images/'+bg_img+'")' );
       }
 
 
-		
+		$('body').bind('touchstart', function() {});
 			
       $("body").delay(300).fadeOut(600, 'swing').promise().done(function() {
        //  $(whichthis).find('#menu').find('.menuon').removeClass('menuon');
 				 
 				 $('#menu').find('.menuon').removeClass('menuon');
-			//	 $('#menu').find('.subitem').css('display','none');
+			  $('#menu').find('.needtap').find('.subitem').css('display','none');
          
 				 $(whichthis).parent().parent().closest('li').addClass('menuon');
          $(whichthis).parent().addClass('menuon');
